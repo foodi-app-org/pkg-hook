8fa2369a5ed4021b126bbe0ed616373799e5bf75
Jesus Juvinao <juvinaojesusd@gmail.com>
2023-09-24 00:38:33 -0500

    feat: edit sub extra optional

---
+++ src/hooks/useEditOneExtProductFoodOptional/queries.js
@@ -0,0 +1,16 @@
+import { gql } from "@apollo/client"
+
+export const UPDATE_EXT_PRODUCT_FOOD_OPTIONAL = gql`
+mutation editExtProductFoodOptional($input: EditExtProductFoodOptionalInput!) {
+    editExtProductFoodOptional(input: $input) {
+      pId
+      opExPid
+      OptionalProName
+      state
+      code
+      required
+      numbersOptionalOnly
+      pDatMod
+    }
+  }
+`
