0b0a338da698a21237a857425e7d14e04386114d
Jesus Juvinao <juvinaojesusd@gmail.com>
2023-10-05 15:42:12 -0500

    feat: update verson

---
+++ src/hooks/handleLogin/index.js
@@ -0,0 +1,10 @@
+export async function handleLogin(body) {
+    const response = await fetch(`${process.env.URL_BASE}/api/auth/login`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+      credentials: 'include'
+    })
+
+    return response.json()
+}
